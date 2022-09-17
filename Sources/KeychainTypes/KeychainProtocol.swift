466f8e07467b1c9f39bd2ab7119dc3aace70a279
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-09-17 17:46:31 -0500

    Removed obsolete functions

--- Sources/KeychainTypes/KeychainProtocol.swift
+++ Sources/KeychainTypes/KeychainProtocol.swift
@@ -10,19 +10,11 @@ import Foundation
 
 public protocol KeychainProtocol: Codable
 {
-    // Key Agreement
-    func generateAndSavePrivateKey(label: String) -> PrivateKey?
-    func retrievePrivateKey(label: String) -> PrivateKey?
+    func generateAndSavePrivateKey(label: String, type: KeyType) -> PrivateKey?
+    func retrievePrivateKey(label: String, type: KeyType) -> PrivateKey?
     func deleteKey(label: String)
 
-    func retrieveOrGeneratePrivateKey(label: String) -> PrivateKey?
+    func retrieveOrGeneratePrivateKey(label: String, type: KeyType) -> PrivateKey?
     func storePrivateKey(_ key: PrivateKey, label: String) -> Bool
     func generateKeySearchQuery(label: String) -> CFDictionary
-
-    // Signing
-    func generateAndSavePrivateSigningKey(label: String) -> PrivateKey?
-    func retrievePrivateSigningKey(label: String) -> PrivateKey?
-
-    func retrieveOrGeneratePrivateSigningKey(label: String) -> PrivateKey?
-    func storePrivateSigningKey(_ key: PrivateKey, label: String) -> Bool
 }
