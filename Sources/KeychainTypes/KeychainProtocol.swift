754e0412ac803c0c5771d10b9903e0adaa04141f
consuelita <adelita@operatorfoundation.org>
2023-07-11 14:47:19 -0500

    Update KeychainProtocol.swift

--- Sources/KeychainTypes/KeychainProtocol.swift
+++ Sources/KeychainTypes/KeychainProtocol.swift
@@ -18,7 +18,7 @@ public protocol KeychainProtocol: Codable
     func retrieveOrGeneratePrivateKey(label: String, type: KeyType) -> PrivateKey?
     func storePrivateKey(_ key: PrivateKey, label: String) -> Bool
 
-    func storePassword(server: String, username: String, password: String) throws
+    func storePassword(server: String, username: String, password: String, overwrite: Bool) throws
     func retrievePassword(server: String) throws -> (username: String, password: String)
     func deletePassword(server: String) throws
 
