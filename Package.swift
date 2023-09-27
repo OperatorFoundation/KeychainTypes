235e023ed18e7738536933dd3f75f201fe38398c
bragelbytes <joebragel@gmail.com>
2023-09-27 14:13:37 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-crypto", from: "2.1.0"),
-        .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", branch: "main")
+        .package(url: "https://github.com/apple/swift-crypto", from: "2.5.0"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", from: "1.2.6")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
