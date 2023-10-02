3bd002644a5d1182dbc39d6dc903b86b91e1d01c
bragelbytes <joebragel@gmail.com>
2023-10-02 11:39:32 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-crypto", from: "2.1.0"),
+        .package(url: "https://github.com/apple/swift-crypto", from: "2.5.0"),
         .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
         .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", branch: "main")
     ],
